8c8ef519bc4b2931ad3ea6baa5e6232d70783af8
rahul-pamula <rahulpamula123@gmail.com>
2026-04-08 17:53:34 +0530

    basic topics

---
+++ java/loop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.Scanner;
+public class loop{
+    public static void main(String[] args)
+{
+    for(int i = 0;i<=10;i++){
+        System.out.println(i);
+    }
+}
+}
